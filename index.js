1ad4eda93b043e46734f661edf98de4dfdeceba9
lesley <lesleybanadzem2017@gmail.com>
2020-01-29 09:46:30 -0800

    initialized npm package.json, installed necessary packages to start up the server, create a simple cript in index.js to start the server, added start script for running app, and included files and folders in the .gitignore to be ignored by git when push up

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = process.env.PORT || 5000
+
+//helmet secures or app by setting various http headers
+app.use(helmet());
+// body-parser parses request bodies to json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//enables clients to make request to our endpoints
+app.use(cors())
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to DevDesk / endpoint')
+})
+
+app.listen(PORT, console.log(`app listening to http://localhost:${PORT}`))
