5540c98858aaa95558107ca70a5df75e022a9114
lesley <lesleybanadzem2017@gmail.com>
2020-01-30 15:53:30 -0800

    Added a post route for adding new tickets by a user

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -4,7 +4,8 @@ const restricted = require("./../middleware/restricted");
 const { fetchUserById } = require("./../models/userModels");
 const {
   fetchTickets,
-  fetchTicketsByUser
+  fetchTicketsByUser,
+  addTicket
 } = require("./../models/ticketsModel");
 
 const route = Router({
@@ -50,5 +51,37 @@ route.get("/:user_id/tickets",  async (req, res, next) => {
     });
   }
 });
+
+
+route.post('/:user_id/tickets' , async(req, res, next) => {
+    try {
+        const user_id = req.params.user_id
+        const user = await fetchUserById(user_id);
+        const { title , description , attempted_solution , completed } = req.body;
+
+        
+        if(!title || !description){
+            return res.status(400).json({
+                message: 'Title and description are requires to add a new ticket'
+            })
+        }else if (user){
+            return res.status(404).json({
+                message: `User at the  {id: ${user_id}} doesn't exist in the db`
+              });
+        }
+        else{
+            const ticket = await addTicket({ title , description , attempted_solution , completed , user_id });
+                  
+            res.status(201).json({
+                message: 'Successfully created',
+                ticket
+
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 
 module.exports = route;
