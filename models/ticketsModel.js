2c98e91706778bf4b57ef71397257ad109d5fb08
lesley <lesleybanadzem2017@gmail.com>
2020-01-31 14:48:56 -0800

    Indent code

--- models/ticketsModel.js
+++ models/ticketsModel.js
@@ -1,32 +1,37 @@
 const db = require("./../data/dbConfig");
-const { fetchUserById, fetchUsers} = require('./userModels');
+const { fetchUserById, fetchUsers } = require("./userModels");
 
 async function fetchTickets() {
   const ticket = await db("tickets");
-
-  const arr = []
+  const arr = [];
   const user = await fetchUsers();
 
-  ticket.forEach((t) => {
-     return user.map(u => (u.id === t.user_id) && arr.push({
-       
+  //map through the ticket array
+  ticket.forEach(t => {
+    //map through the user array
+    return user.map(
+      u =>
+        // if ticket.user_id is equal to the user.id them we push into the arr created 
+        u.id === t.user_id &&
+        arr.push({
           ticket_id: t.ticket_id,
           title: t.title,
           submitted_by: u.full_name,
           description: t.description,
           attempted_solution: t.attempted_solution,
           created_at: t.created_at,
-          completed: t.completed,
-          })
-        )
-  })
+          completed: t.completed
+        })
+    );
+  });
 
-  return arr
+  return arr;
 }
 
 async function fetchTicketsByUser(id) {
-
-  const user = await db("user").where("id", id).first();
+  const user = await db("user")
+    .where("id", id)
+    .first();
   const tickets = await db("tickets").where("user_id", id);
 
   return {
@@ -38,8 +43,10 @@ async function fetchTicketsByUser(id) {
 }
 
 async function addTicket(ticket) {
-    const t = await db('tickets').insert(ticket);
-    return db('tickets').where('title', ticket.title).first();
+  const t = await db("tickets").insert(ticket);
+  return db("tickets")
+    .where("title", ticket.title)
+    .first();
 }
 module.exports = {
   fetchTickets,
